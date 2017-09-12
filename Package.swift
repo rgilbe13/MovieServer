53f21307cb8fe40cf829c57f923157b60dd638f4
Rob Gilbert <rob@Robs-MacBook-Pro.local>
2017-09-12 18:31:33 -0400

    Heroku

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         Target(name: "Run", dependencies: ["App"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/sqlite.git", majorVersion: 2)
     ],
